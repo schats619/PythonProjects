6f5ee292ff7377e1251d3d5170d45493913ec980
Sourav Chatterjee <sourav.c619@gmail.com>
2017-11-11 13:41:11 -0500

    solution for 3a. Finding range

---
+++ Chapter 3/3a.py
@@ -0,0 +1,18 @@
+def testrange():
+    for i in range(5):
+        print(i)
+
+def testrange1():
+    for i in range(3, 10):
+        print(i)
+
+def testrange2():
+    for i in range(4,13,3):
+        print(i)
+
+options = {0: testrange,
+            1: testrange1,
+            2: testrange2,
+}
+
+options[0] ()
