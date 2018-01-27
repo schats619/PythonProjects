d29c1407e20609d56ef1f3c6250d9286e2221f07
Sourav Chatterjee <sourav.c619@gmail.com>
2018-01-27 12:34:56 -0500

    Solution for ex4(b).py

---
+++ Chapter 3/ex4(b).py
@@ -0,0 +1,9 @@
+#Solution: 1**3, 3**3, 5**3, 7**3, 9**3
+#the output will return the last index of the array
+
+def range2():
+    for i in [1,3,5,7,9]:
+        print(i, ":", i**3)
+    print("The range is ", i)
+
+range2()
